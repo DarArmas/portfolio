dcea050766b48e75d6fa12fd7bbe21b6a750f25f
Dar <darnellsanchez2011@gmail.com>
2023-03-21 00:03:52 -0600

    fix: test without homepage

--- src/components/cover/Cover.js
+++ src/components/cover/Cover.js
@@ -9,7 +9,7 @@ const Cover = () => {
             <h1>Darnell Armas</h1>
             <p>Software developer</p>
             <a
-                href="/portfolio/assets/CV.pdf"
+                href="assets/CV.pdf"
                 target="_blank"
                 rel="noreferrer"
                 className="btn btn-primary mt-2"
